ac8c494053016d01b6776c5d90d2e4eed023c571
Jeferson GB <jefersongb@outlook.com.br>
2020-02-27 22:30:31 -0300

    Create new pages.

---
+++ src/pages/about.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import Layout from "../components/layout"
+
+
+const AboutPage = () => {
+  return (
+    <Layout>
+    <div>
+      <h1>About Us</h1>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. At alias culpa tempore provident, doloribus minima in nobis veniam ea sapiente aliquid, vero doloremque mollitia rem debitis pariatur voluptatem cupiditate facere. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sed, possimus!</p>
+    
+    </div>
+    </Layout>
+  )
+}
+
+export default AboutPage;
---
+++ src/pages/services.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import Layout from "../components/layout"
+
+
+const ServicesPage = () => {
+  return (
+    <Layout>
+    <div>
+      <h1>Our Services</h1>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. At alias culpa tempore provident, doloribus minima in nobis veniam ea sapiente aliquid, vero doloremque mollitia rem debitis pariatur voluptatem cupiditate facere. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sed, possimus!</p>
+    
+    </div>
+    </Layout>
+  )
+}
+
+export default ServicesPage;
